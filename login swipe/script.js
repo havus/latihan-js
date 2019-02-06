9d1c48a4a3b91e54169391e7e84da263e4077d11
havus <havus.it@gmail.com>
2019-02-06 13:58:06 +0700

    tambah css

--- login swipe/script.js
+++ login swipe/script.js
@@ -1,5 +1,5 @@
-let $id = id => document.getElementById(id);
-var [login, register, form] = ["login", "register", "form"].map(id => $id(id));
+let $id = (id) => document.getElementById(id);
+var [login, register, form] = ['login', 'register', 'form'].map(id => $id(id));
 
 [login, register].map(element => {
     element.onclick = function () {
@@ -7,8 +7,6 @@ var [login, register, form] = ["login", "register", "form"].map(id => $id(id));
             $ele.classList.remove("active");
         });
         this.classList.add("active");
-        this.getAttribute("id") === "register" ?
-            form.classList.add("active") :
-            form.classList.remove("active");
-    };
+        this.getAttribute("id") === "register" ? form.classList.add("active") : form.classList.remove("active");
+    }
 });
