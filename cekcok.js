31c580b4e790f94b4df454aeb4db043777ccfcc8
root <root@localhost.localdomain>
2019-04-29 20:30:50 +0700

    cekcok

---
+++ cekcok.js
@@ -0,0 +1,22 @@
+// fungsi kurung adalah func yg dijalankan ketika html sudah ready
+// (function() {
+//     $('h1').addClass('tittle');
+// })()
+
+let countClick = 0;
+
+$('#pencet').on('click', function() {
+    // e.preventDefault();
+    if (countClick == 0) {
+        $('h1').fadeIn(1000);
+        $('h1').removeClass('tittle');
+    } else if (countClick == 1){
+        $('h1').addClass('tittle');
+    } else {
+        $('h1').fadeOut(1000);
+        countClick = -1;
+    }
+    console.log(countClick);
+    countClick++;
+})
+
